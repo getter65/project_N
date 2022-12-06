af95d01cf5796b8c608112d063e8f52d5eba4fd5
Nimmeron <maxic499@gmail.com>
2022-12-07 04:36:54 +0500

    added functions

---
+++ testing_approaches/testing_approaches/get.py
@@ -0,0 +1,8 @@
+def get(coll, index, default=None):
+    if index >= len(coll) or index < 0:
+        if default is not None:
+            return default
+        return None
+
+    return coll[index]
+
---
+++ testing_approaches/tests/test_slice.py
@@ -0,0 +1,2 @@
+def test_slice():
+    assert my_slice([1, 2, 3, 4, 5, 6], 1, 4) == [2, 3, 4]
