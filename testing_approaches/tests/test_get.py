d77ab9064891952a36d86888faf3b4c0a7f61ae5
Nimmeron <maxic499@gmail.com>
2022-12-07 04:34:03 +0500

    added functions and tests

---
+++ testing_approaches/tests/test_get.py
@@ -0,0 +1,3 @@
+def test_get():
+    assert get([1, 2, 3], 1, "a") == 2
+    assert get([4, 5, 6], 7, "val") == "val"
---
+++ testing_approaches/tests/test_index_of.py
@@ -0,0 +1,2 @@
+def test_index_of():
+    assert index_of([2, 7, 3, 2, 4], 2) == 0
