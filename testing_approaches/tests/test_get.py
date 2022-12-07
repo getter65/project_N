8fe28bd3b8391a988116d15f04e0524e58767f3b
Nimmeron <maxic499@gmail.com>
2022-12-07 05:22:45 +0500

    fix bag

--- testing_approaches/tests/test_get.py
+++ testing_approaches/tests/test_get.py
@@ -3,4 +3,4 @@ from testing_approaches.get import get
 def test_get():
     assert get([1, 2, 3], 1, "a") == 2
     assert get([4, 5, 6], 7, "val") == "val"
-    assert get([7, 8, 9], -1, "greep") == None
+    assert get([7, 8, 9], -1) == None
