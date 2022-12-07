c2bd8e640d52186a1ce6e994d9bdb1ba8c11a14b
Nimmeron <maxic499@gmail.com>
2022-12-07 05:18:14 +0500

    added tests function

--- testing_approaches/tests/test_index_of.py
+++ testing_approaches/tests/test_index_of.py
@@ -2,3 +2,6 @@ from testing_approaches.index_of import index_of
 
 def test_index_of():
     assert index_of([2, 7, 3, 2, 4], 2) == 0
+    assert index_of([], 1) == -1
+    assert index_of([1, 1, 1], 2) == -1
+    assert index_of([1, 3, 4, 2], 4) == 2
--- testing_approaches/tests/test_slice.py
+++ testing_approaches/tests/test_slice.py
@@ -2,3 +2,6 @@ from testing_approaches.slice import my_slice
 
 def test_slice():
     assert my_slice([1, 2, 3, 4, 5, 6], 1, 4) == [2, 3, 4]
+    assert my_slice([], 1, 4) == []
+    assert my_slice([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+    assert my_slice([1, 2, 3, 4, 5, 6], 0, 3) == [1, 2, 3]
